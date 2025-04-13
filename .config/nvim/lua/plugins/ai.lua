4185abfa10beeead3fc60ac4e29de91f51df9a18
Rick Choi <jinhwanlazy@gmail.com>
2025-04-13 18:39:58 +0900

    Use parrot instead of codecompanion

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -42,58 +42,77 @@ return {
         end,
     },
     {
-        "olimorris/codecompanion.nvim",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-treesitter/nvim-treesitter",
-        },
+        "frankroeder/parrot.nvim",
+        dependencies = { "ibhagwan/fzf-lua", "nvim-lua/plenary.nvim" },
+        opts = {},
         config = function()
-            require("codecompanion").setup({
-                strategies = {
-                    chat = {
-                        adapter = "anthropic",
-                        keymaps = {
-                            close = {
-                                modes = { n = "<C-x>", i = "<C-x>" },
-                            },
-                        },
-                    },
-                    inline = {
-                        adapter = "anthropic",
+            require("parrot").setup {
+                -- Providers must be explicitly added to make them available.
+                providers = {
+                    anthropic = {
+                        api_key = os.getenv "ANTHROPIC_API_KEY",
                     },
                 },
-                adapters = {
-                    openai_o3_mini = function()
-                        return require("codecompanion.adapters").extend("openai", {
-                            name = "openai_o3_mini",
-                            schema = {
-                                model = {
-                                    default = "o3-mini-2025-01-31",
-                                },
-                            },
-                        })
+                chat_shortcut_respond = { modes = { "n", "i", "v", "x" }, shortcut = "<C-g><C-g>" },
+                chat_shortcut_delete = { modes = { "n", "i", "v", "x" }, shortcut = "<C-g>d" },
+                chat_shortcut_stop = { modes = { "n", "i", "v", "x" }, shortcut = "<C-g>s" },
+                chat_shortcut_new = { modes = { "n", "i", "v", "x" }, shortcut = "<C-g>c" },
+                show_thinking_window = false,
+                hooks = {
+                    Complete = function(prt, params)
+                        local template = [[
+                            I have the following code from {{filename}}:
+
+                            ```{{filetype}}
+                            {{selection}}
+                            ```
+
+                            Please finish the code above carefully and logically.
+                            Respond just with the snippet of code that should be inserted."
+                            ]]
+                        local model_obj = prt.get_model "command"
+                        prt.Prompt(params, prt.ui.Target.append, model_obj, nil, template)
                     end,
-                    openai_o1_mini = function()
-                        return require("codecompanion.adapters").extend("openai", {
-                            name = "openai_o1_mini",
-                            schema = {
-                                model = {
-                                    default = "o1-mini-2024-09-12",
-                                },
-                            },
-                        })
+                    CodeConsultant = function(prt, params)
+                        local chat_prompt = [[
+                            Your task is to analyze the provided {{filetype}} code and suggest
+                            improvements to optimize its performance. Identify areas where the
+                            code can be made more efficient, faster, or less resource-intensive.
+                            Provide specific suggestions for optimization, along with explanations
+                            of how these changes can enhance the code's performance. The optimized
+                            code should maintain the same functionality as the original code while
+                            demonstrating improved efficiency.
+
+                            Here is the code
+                            ```{{filetype}}
+                            {{filecontent}}
+                            ```
+                        ]]
+                        prt.ChatNew(params, chat_prompt)
                     end,
-                },
-            })
+                }
+            }
+
+            local function keymapOptions(desc)
+                return {
+                    noremap = true,
+                    silent = true,
+                    nowait = true,
+                    desc = "GPT prompt " .. desc,
+                }
+            end
+            vim.keymap.set("n", "<C-g>q", "<cmd>PrtAsk<cr>", keymapOptions("Ask a question"))
+            vim.keymap.set("n", "<C-g>a", "<cmd>PrtAppend<cr>", keymapOptions("Append"))
+
+            vim.keymap.set({"n", "i"}, "<C-g>c", "<cmd>PrtChatNew<cr>", keymapOptions("New Chat"))
+            vim.keymap.set({"n", "i"}, "<C-g>t", "<cmd>PrtChatToggle<cr>", keymapOptions("Toggle Chat"))
+            vim.keymap.set({"n", "i"}, "<C-g>f", "<cmd>PrtChatFinder<cr>", keymapOptions("Chat Finder"))
+
+            vim.keymap.set("v", "<C-g>c", ":<C-u>'<,'>PrtChatNew<cr>", keymapOptions("Visual Chat New"))
+            vim.keymap.set("v", "<C-g>p", ":<C-u>'<,'>PrtChatPaste<cr>", keymapOptions("Visual Chat Paste"))
+            vim.keymap.set("v", "<C-g>t", ":<C-u>'<,'>PrtChatToggle<cr>", keymapOptions("Visual Toggle Chat"))
+            vim.keymap.set("v", "<C-g>i", ":<C-u>'<,'>PrtComplete<cr>", keymapOptions("Complete visual selection"))
+            vim.keymap.set("v", "<C-g>a", ":<C-u>'<,'>PrtAppend<cr>", keymapOptions("Visual Append"))
         end,
-        keys = {
-            { "<C-g>q", ":CodeCompanion<cr>", mode="n", },
-            { "<C-g>q", ":CodeCompanion ", mode="v", },
-            { "<C-g>n", ":CodeCompanionChat<cr>", mode="n", },
-            { "<C-g>t", ":CodeCompanionChat Toggle<cr>", mode="n", },
-            { "<C-g>t", ":CodeCompanionChat Add<cr>", mode="v", },
-            { "<C-g>a", ":CodeCompanionActions<cr>", mode="n", },
-            { "<C-g>c", ":CodeCompanionCmd ", mode="n", },
-        },
     },
 }
