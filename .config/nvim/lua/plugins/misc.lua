435da0255d1b13bfa74bf7a0aaac59f55c94fbf4
Rick Choi <jinhwanlazy@gmail.com>
2025-05-01 15:20:42 +0900

    add textobj plugin

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -44,18 +44,13 @@ return {
             require('log-highlight').setup {}
         end,
     },
-    --{ 
-        --'ojroques/nvim-osc52',
-        --config = function()
-            --require('osc52').setup {
-                --max_length = 0,           -- Maximum length of selection (0 for no limit)
-                --silent = false,           -- Disable message on successful copy
-                --trim = false,             -- Trim surrounding whitespaces before copy
-                --tmux_passthrough = false, -- Use tmux passthrough (requires tmux: set -g allow-passthrough on)
-            --}
-        --keys = { 
-            --{ "<leader>y", require('osc52').copy_visual, mode = "v" }, 
-        --}
-        --end,
-    --},
+    {
+        'chrisgrieser/nvim-various-textobjs',
+        event = "VeryLazy",
+        opts = { 
+            keymaps = {
+                useDefaults = true 
+            }
+        },
+    },
 }
