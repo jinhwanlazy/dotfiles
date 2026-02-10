1a8c0516b0e7ab225075fcd2ede4043a695dc7bf
Rick Choi <rick.choi@cupix.com>
2026-02-10 12:43:12 +0900

    fix ClangdSwitchSourceHeader command not working

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -33,6 +33,18 @@ return {
                     if client.server_capabilities.documentSymbolProvider then
                         require("nvim-navic").attach(client, bufnr)
                     end
+                    -- Register clangd-specific commands
+                    vim.api.nvim_buf_create_user_command(bufnr, 'ClangdSwitchSourceHeader', function()
+                        local params = vim.lsp.util.make_text_document_params(bufnr)
+                        client:request('textDocument/switchSourceHeader', params, function(err, result)
+                            if err then error(tostring(err)) end
+                            if not result then
+                                vim.notify('Corresponding file cannot be determined')
+                                return
+                            end
+                            vim.cmd.edit(vim.uri_to_fname(result))
+                        end, bufnr)
+                    end, { desc = 'Switch between source/header' })
                 end
             })
         end,
